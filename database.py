81f321a962c6fac7b62cf104ebecef7176bde532
Maxim <flipsidebeat@gmail.com>
2022-05-21 20:14:43 +0300

    Optimized_base

--- database.py
+++ database.py
@@ -1,2 +1,10 @@
-login = 'flipsidebeats'
-password = 'DminornoMusicStArs56'
+username1 = 'macsim05.macsim@mail.ru'
+password1 = 'Vevahu51'
+
+
+username = 'flipsud'
+password = 'vevahu51'
+
+
+username2 = 'flipsidebeats'
+password2 = 'DminornoMusicStArs56'
