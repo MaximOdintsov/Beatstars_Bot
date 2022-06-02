2095a1d7dded4ba05b43eca1fcf320563de3ceb8
Maxim <flipsidebeat@gmail.com>
2022-06-02 20:22:24 +0300

    Teletest5

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import os
 
-from BeatstarsBot import Beatstars_Bot
+# from BeatstarsBot import Beatstars_Bot
 import telebot
-import logging
 from flask import Flask, request
-
 
 BOT_TOKEN = "5411402973:AAGhVaeouBYSFIAGOHshoBBMak3poZ5HxKI"
 BOT_URL = "https://beatstarsbot.herokuapp.com/"
 
-
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
-
-
-# if __name__ == "__main__":
-#     bot.remove_webhook()
-#     bot.set_webhook(url=APP_URL)
-#     server.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    username = message.from_user.username
-    bot.reply_to(message, f"Hello, {username}!")
+    bot.reply_to(message, "Hello")
 
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+@server.route('/' + BOT_TOKEN, methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
-        
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=BOT_URL)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 # print('Введите логин: ')
 # username = input()
